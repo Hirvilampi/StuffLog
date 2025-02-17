f2ef1bef90245f4668ad85019d2bf58c37f5167a
Hirvilampi <bhr611@myy.haaga-helia.fi>
2025-02-17 15:58:01 +0200

    stufflog projekti luotu - saas nähdä toimiiko, kun tein väärässä järjestyksessä

---
+++ stufflog/src/main/java/kevat25/stufflog/StufflogApplication.java
@@ -0,0 +1,13 @@
+package kevat25.stufflog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StufflogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StufflogApplication.class, args);
+	}
+
+}
---
+++ stufflog/src/test/java/kevat25/stufflog/StufflogApplicationTests.java
@@ -0,0 +1,13 @@
+package kevat25.stufflog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StufflogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
